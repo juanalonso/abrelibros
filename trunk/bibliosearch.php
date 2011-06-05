1d78d22b82dda548e128206f9aa52fbeb5f0f7bb
juanalonso@gmail.com <juanalonso@gmail.com@f574a77e-250f-abf8-3f5d-9f2ed02557dd>
2011-06-05 13:41:44 +0000

    Mejoras post entrega de premios

--- trunk/bibliosearch.php
+++ trunk/bibliosearch.php
@@ -34,7 +34,15 @@
   <h2>Añade los libros a tu lista o ve a buscarlos</h2>
   <div id='searchlist'><img src='img/progress.gif'/>Cargando resultados...</div>
   <div class='line'></div>
-  <div class='flright'><a class='button blue' href='main.php'>volver</a></div>
+  <div class="space"><br clear="all"/></div>
+  <h3>¿Quieres buscar más libros?</h3>
+  <form action="bibliosearch.php">
+    <input type="text" placeholder="Busca el libro por autor, título, editorial, isbn..." name="searchtext"/>
+    <input class="button blue" type="submit" value="buscar"/>
+    <br/>
+    <input class="button white flright" style="margin-right:7px;" type="submit" value="volver"/>
+  </form>
+  <div class="space"><br clear="all"></div>
 FIN;
     
   
